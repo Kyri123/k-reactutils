fb9f09b105b95ecba6778afed68452fa5110c58c
Oliver <olli143@live.de>
2023-04-02 13:14:00 +0200

    fix useToggle types

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -6,7 +6,10 @@ import {
  * Toggle hook to toggle a boolean.
  * Good for modals or something like that
  */
-export function useToggle( InitValue: boolean ) {
+export function useToggle( InitValue: boolean ): [
+	boolean,
+	() => void
+] {
 	const [ Boolean, setBoolean ] = useState<boolean>( InitValue );
 	const Toggle = () => setBoolean( Current => !Current );
 
